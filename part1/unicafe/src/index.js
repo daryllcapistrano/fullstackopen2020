2529df6d4a4f96712ac9f4bb0aac7becb40a5948
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-05-26 14:35:06 -0700

    completed 1.11*: unicafe step6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,17 +3,35 @@ import ReactDOM from 'react-dom';
 
 const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>;
 
-const Statistics = ({ text, value }) => (
-	<p>
-		{text}: {value}
-	</p>
-);
+// const Statistics = ({ text, value }) => (
+// 	<tr>
+// 		<td>{text}</td>
+// 		<td>{value}</td>
+// 	</tr>
+// );
+
+const Statistics = (props) => {
+	if (props.value === 0) {
+		return (
+			<tr>
+				<td>{props.text}</td>
+				<td>0</td>
+			</tr>
+		);
+	} else {
+		return (
+			<tr>
+				<td>{props.text}</td>
+				<td>{props.value}</td>
+			</tr>
+		);
+	}
+};
 
 const App = () => {
 	const [ good, setGood ] = useState(0);
 	const [ neutral, setNeutral ] = useState(0);
 	const [ bad, setBad ] = useState(0);
-
 	const [ total, setAll ] = useState(0);
 
 	const incrementGood = () => {
@@ -38,17 +56,23 @@ const App = () => {
 
 	return (
 		<div>
-			<h1>give feedback</h1>
+			<h1>Give Feedback</h1>
 			<Button handleClick={incrementGood} text="good" />
 			<Button handleClick={incrementNeutral} text="neutral" />
 			<Button handleClick={incrementBad} text="bad" />
-			<h2>statistics</h2>
-			<Statistics text="good" value={good} />
-			<Statistics text="neutral" value={neutral} />
-			<Statistics text="bad" value={bad} />
-			<Statistics text="total" value={total} />
-			<Statistics text="average" value={average} />
-			<Statistics text="percent" value={posPercent} />
+			<br />
+			{/* for spacing */}
+			<br />
+			<table>
+				<tbody>
+					<Statistics text="good" value={good} />
+					<Statistics text="neutral" value={neutral} />
+					<Statistics text="bad" value={bad} />
+					<Statistics text="total" value={total} />
+					<Statistics text="average" value={average} />
+					<Statistics text="percent" value={posPercent} />
+				</tbody>
+			</table>
 		</div>
 	);
 };
