78b7393cc3c209c6ceff876edc1ef8cf66bcf125
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-05-26 13:20:50 -0700

    1.9: unicafe step4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,50 +1,77 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = ({ text }) => {
-	return (
-		<div>
-			<h1>{text}</h1>
-		</div>
-	);
-};
+const Header = (props) => (
+	<div>
+		<h1>{props.text}</h1>
+	</div>
+);
 
-const Statistics = ({ text, good, neutral, bad }) => {
-	return (
-		<React.Fragment>
-			<h2>{text}</h2>
-			<div>Good...{good}</div>
-			<div>Neutral...{neutral}</div>
-			<div>Bad...{bad}</div>
-			{/* implement total here */}
-		</React.Fragment>
-	);
-};
+const FeedbackButton = (props) => <button onClick={props.handleClick}>{props.text}</button>;
 
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+const Statistics = (props) => {
+	if (props.total === 0) {
+		return <p>No feedback given</p>;
+	} else {
+		return (
+			<React.Fragment>
+				<h2>{props.text}</h2>
+				<div>Good...{props.good}</div>
+				<div>Neutral...{props.neutral}</div>
+				<div>Bad...{props.bad}</div>
+				<strong>Total... {props.total}</strong>
+				<div>Average...{props.average}</div>
+				<div>Positive Feedback {props.percent} % </div>
+			</React.Fragment>
+		);
+	}
+};
 
 const App = () => {
 	const headerText = 'give feeback';
 	const statisticsText = 'statistics';
-	// save clicks of each button to own state
+
 	const [ good, setGood ] = useState(0);
 	const [ neutral, setNeutral ] = useState(0);
 	const [ bad, setBad ] = useState(0);
-	// console.log('good count...', good);
-	// console.log('neutral count...', neutral);
-	// console.log('bad count...', bad);
 
-	const incrementGood = () => setGood(good + 1);
-	const incrementNeutral = () => setNeutral(neutral + 1);
-	const incrementBad = () => setBad(bad + 1);
+	const [ total, setAll ] = useState(0);
+
+	const incrementGood = () => {
+		setGood(good + 1);
+		setAll(total + 1);
+	};
+
+	const incrementNeutral = () => {
+		setNeutral(neutral + 1);
+		setAll(total + 1);
+	};
+	const incrementBad = () => {
+		setBad(bad + 1);
+		setAll(total + 1);
+	};
+
+	let values = [ good, neutral, bad ];
+	let sum = values.reduce((previous, current) => (current += previous));
+	let average = sum / values.length;
+
+	let percent = isNaN(good / total) ? '0' : good / total * 100;
 
 	return (
 		<div>
 			<Header text={headerText} />
-			<Button handleClick={incrementGood} text="good" />
-			<Button handleClick={incrementNeutral} text="neutral" />
-			<Button handleClick={incrementBad} text="bad" />
-			<Statistics text={statisticsText} good={good} neutral={neutral} bad={bad} />
+			<FeedbackButton handleClick={incrementGood} text="good" />
+			<FeedbackButton handleClick={incrementNeutral} text="neutral" />
+			<FeedbackButton handleClick={incrementBad} text="bad" />
+			<Statistics
+				text={statisticsText}
+				good={good}
+				neutral={neutral}
+				bad={bad}
+				total={total}
+				average={average}
+				percent={percent}
+			/>
 		</div>
 	);
 };
